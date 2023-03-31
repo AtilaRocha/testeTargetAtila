a8bf5a42d9e983576f11b53531ffc327bd39f627
Atila Rocha <72106377+AtilaRocha@users.noreply.github.com>
2023-03-31 19:15:46 -0300

    Corrigindo Questão 3 com dados json enviados corretamente.

--- quest03/quest03.py
+++ quest03/quest03.py
@@ -1,29 +1,14 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('G:/Atila_Rocha/Programacao/testeTarget/quest03/faturamento.xml')
-root = tree.getroot()
-menor_valor = float('inf')
-maior_valor = 0
-total_faturamento = 0
-total_dias = 0
-for mes in root.findall('mes'):
-    faturamento_mes = 0
-    dias_mes = 0
-    for dia in mes.findall('dia'):
-        valor = float(dia.get('valor'))
-        if valor < menor_valor:
-            menor_valor = valor
-        if valor > maior_valor:
-            maior_valor = valor
-        faturamento_mes += valor
-        dias_mes += 1
-    media_mes = faturamento_mes / dias_mes
-    for dia in mes.findall('dia'):
-        valor = float(dia.get('valor'))
-        if valor > media_mes:
-            total_dias += 1
-    total_faturamento += faturamento_mes
-    total_dias += dias_mes
-media_geral = total_faturamento / total_dias
-print(f"Menor valor de faturamento: R${menor_valor:.2f}")
-print(f"Maior valor de faturamento: R${maior_valor:.2f}")
-print(f"Número de dias com faturamento acima da média mensal: {total_dias}")
+import json
+with open('G:/Atila_Rocha/Programacao/testeTarget/quest03/dados.json') as f:
+    data = json.load(f)
+menor_valor = sorted(data, key=lambda x: x['valor'])[0]['valor']
+maior_valor = sorted(data, key=lambda x: x['valor'], reverse=True)[0]['valor']
+total_faturamento = sum([dia["valor"] for dia in data])
+media_mensal = total_faturamento / len(data)
+dias_acima_media = 0
+for dia in data:
+    if dia["valor"] > media_mensal:
+        dias_acima_media += 1
+print(f"Menor valor de faturamento ocorrido em um dia do mês: R$ {menor_valor:.2f}")
+print(f"Maior valor de faturamento ocorrido em um dia do mês: R$ {maior_valor:.2f}")
+print(f"O total de dias acima da Média Mensal ({media_mensal:.2f}): {dias_acima_media} dias")
